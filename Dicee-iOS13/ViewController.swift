72ffa1adad1ec6db91f543026b8cb419080e1d11
Prashant Bhat <prashantbhat84@gmail.com>
2024-01-06 18:02:37 +0530

    changing the dice image on load

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,19 +2,26 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
+//  Created by Prashant Bhat on 11/06/2019.
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // image literal for changing the image on the fly
+        diceImageViewOne.image = UIImage(imageLiteralResourceName:"DiceSix")
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
+        diceImageViewOne.alpha = 0.5
+        diceImageViewTwo.alpha = 0.3
     }
-
-
+    
 }
 
